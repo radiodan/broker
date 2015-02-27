d95edba877cefe4c77b90343ee9aed9e51ee3d67
Dan Nuttall <pixelblend@gmail.com>
2015-02-27 12:44:07 +0000

    Add service directory to broker.
    Needs hefty refactoring

--- broker.go
+++ broker.go
@@ -1,10 +1,94 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	zmq "github.com/pebbe/zmq4"
 	"log"
 	"time"
 )
+
+type ServiceDirectory struct {
+	services map[string]*ServiceType
+	index    map[string][]string
+	workers  map[string]*Worker
+}
+
+type ServiceType struct {
+	name      string
+	instances map[string]*Worker
+}
+
+type Worker struct {
+	name     string // human readable id
+	identity string // routing frame
+}
+
+func (serviceDirectory *ServiceDirectory) AddWorker(identity string, serviceTypeName string, serviceInstanceName string) {
+	// create or return worker
+	worker, exists := serviceDirectory.workers[identity]
+
+	if exists == false {
+		name := fmt.Sprintf("%q", identity)
+		worker = &Worker{
+			identity: identity,
+			name:     name,
+		}
+
+		serviceDirectory.workers[identity] = worker
+	}
+
+	log.Printf("?: Worker - %q", worker)
+
+	// create or return serviceType
+	serviceType, exists := serviceDirectory.services[serviceTypeName]
+
+	if exists == false {
+		serviceType = &ServiceType{
+			name:      serviceTypeName,
+			instances: make(map[string]*Worker),
+		}
+
+		serviceDirectory.services[serviceTypeName] = serviceType
+	}
+
+	// register serviceInstance, or err if already found
+	_, exists = serviceType.instances[serviceInstanceName]
+
+	if exists == false {
+		log.Printf("Adding worker %q to %s.%s", worker, serviceTypeName, serviceInstanceName)
+		serviceType.instances[serviceInstanceName] = worker
+	}
+
+	// add to index
+	serviceDirectory.index[serviceTypeName] = append(serviceDirectory.index[serviceTypeName], serviceInstanceName)
+}
+
+func (serviceDirectory *ServiceDirectory) WorkerForService(serviceTypeName string, serviceInstanceName string) (serviceWorker *Worker, err error) {
+	// check for serviceType
+	serviceType, exists := serviceDirectory.index[serviceTypeName]
+
+	if !exists {
+		err = errors.New("Unknown serviceType")
+		return
+	}
+
+	log.Printf("Found serviceType %q", serviceType)
+
+	for _, serviceInstance := range serviceType {
+		if serviceInstance == serviceInstanceName {
+			log.Printf("Match: %q", serviceDirectory.services[serviceTypeName])
+			serviceWorker, exists = serviceDirectory.services[serviceTypeName].instances[serviceInstanceName]
+			if exists {
+				log.Printf("Found serviceWorker: %q", serviceWorker)
+				return
+			}
+		}
+	}
+
+	err = errors.New("Unknown serviceInstance")
+	return
+}
 
 func main() {
 	// create connection socket
@@ -27,7 +111,11 @@ func main() {
 
 	log.Println("Now listening at: " + location)
 
-	workers := map[string]string{}
+	serviceDirectory := ServiceDirectory{
+		services: make(map[string]*ServiceType),
+		workers:  make(map[string]*Worker),
+		index:    make(map[string][]string),
+	}
 
 	for {
 		polled, err := poller.Poll(time.Second * 10)
@@ -52,8 +140,12 @@ func main() {
 
 			if protocol == "MDPW02" && command == "1" {
 				log.Printf("I: %s is a worker\n", sender)
-				workers[protocol] = sender
-				log.Printf("I: workers - %q\n", workers)
+				serviceType := data[0]
+				for _, serviceInstance := range data[1:] {
+					log.Printf("?: %q\n", serviceInstance)
+					serviceDirectory.AddWorker(sender, serviceType, serviceInstance)
+				}
+				log.Printf("I: services - %q\n", serviceDirectory.index)
 			}
 
 			if protocol == "MDPW02" && command == "2" {
@@ -65,23 +157,33 @@ func main() {
 
 			if protocol == "MDPC02" && command == "1" {
 				log.Printf("I: %s is a client\n", sender)
-				if len(workers) > 0 {
-					// send message to worker, don't care about services yet
-					log.Printf("I: sending data to worker %s", workers["MDPW02"])
-					msgCount, err := socket.SendMessage(workers["MDPW02"], sender, data)
-					if err != nil {
-						log.Printf("! %x", err)
-					} else {
-						log.Printf("I: sent %i bytes", msgCount)
-					}
+				log.Printf("I: data - %q", data)
+
+				//correlationId := data[0]
+				serviceType := data[1]
+				serviceInstance := data[2]
+				//msg := data[3:]
+
+				worker, err := serviceDirectory.WorkerForService(serviceType, serviceInstance)
+
+				if err != nil {
+					log.Printf("I: No worker for %s.%s", serviceType, serviceInstance)
+					return
+				}
+				log.Printf("I: sending data to worker %s", worker.name)
+				msgCount, err := socket.SendMessage(worker.identity, sender, data)
+				if err != nil {
+					log.Printf("! %x", err)
+				} else {
+					log.Printf("I: sent %i bytes", msgCount)
 				}
 			}
 
-			log.Printf("I: received message: %q\n", msg)
-			log.Printf("I: sender: %q\n", sender)
-			log.Printf("I: protocol: %q\n", protocol)
-			log.Printf("I: command: %q\n", command)
-			log.Printf("I: data: %q\n", data)
+			//log.Printf("I: received message: %q\n", msg)
+			//log.Printf("I: sender: %q\n", sender)
+			//log.Printf("I: protocol: %q\n", protocol)
+			//log.Printf("I: command: %q\n", command)
+			//log.Printf("I: data: %q\n", data)
 		}
 	}
 }
