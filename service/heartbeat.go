08710ca39a6f1cb99c6ed8af8ab6dec5cf66f432
Dan Nuttall <pixelblend@gmail.com>
2015-03-04 16:06:00 +0000

    Remove TODO

--- service/heartbeat.go
+++ service/heartbeat.go
@@ -6,7 +6,6 @@ import (
 
 func (b *Broker) Heartbeat() {
 	for _, worker := range b.Service.workers {
-		// TODO: Fix message
 		b.Socket.SendMessage(worker.Identity, COMMAND_HEARTBEAT, []string{})
 	}
 	b.HeartbeatAt = time.Now().Add(HEARTBEAT_INTERVAL)
