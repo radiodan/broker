81af685dfd2dcf60f2498ce3d29cc828223b8fa6
Dan Nuttall <pixelblend@gmail.com>
2015-04-01 11:56:21 +0100

    Add flags for broker endpoints
    Each of the three endpoints can be set to a port number or to a socket
    path. If a path is set, that is given priority over the port if both are
    set.
    
    The defaults are port numbers `7171:7173`.

--- broker.go
+++ broker.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/radiodan/broker/pubsub"
 	"github.com/radiodan/broker/service"
 	"log"
+	"path"
 )
 
 func main() {
-	serviceLocation := "tcp://0.0.0.0:7171"
+	serviceLocation, pubLocation, subLocation := parseFlags()
+
 	serviceBroker := service.New(serviceLocation)
-
-	pubLocation := "tcp://0.0.0.0:7172"
-	subLocation := "tcp://0.0.0.0:7173"
-
 	pubSubBroker := pubsub.New(pubLocation, subLocation)
 
 	go serviceBroker.Poll()
@@ -25,4 +25,34 @@ func main() {
 	// cheap trick to keep the main thread running
 	forever := make(chan bool)
 	<-forever
+}
+
+func parseFlags() (service string, pub string, sub string) {
+	servicePort := flag.Int("service-port", 7171, "Port for service")
+	pubPort := flag.Int("pub-port", 7172, "Port for publishing")
+	subPort := flag.Int("sub-port", 7173, "Port for subscribing")
+
+	serviceSocket := flag.String("service-socket", "", "Socket path for service")
+	pubSocket := flag.String("pub-socket", "", "Socket path for publishing")
+	subSocket := flag.String("sub-socket", "", "Socket path for subscribing")
+
+	flag.Parse()
+
+	service = connectionPath(servicePort, serviceSocket)
+	pub = connectionPath(pubPort, pubSocket)
+	sub = connectionPath(subPort, subSocket)
+
+	return
+}
+
+func connectionPath(port *int, socket *string) (fullPath string) {
+	switch {
+	case *socket == "":
+		fullPath = fmt.Sprintf("tcp://0.0.0.0:%v", *port)
+	default:
+		socketPath := path.Clean(*socket)
+		fullPath = fmt.Sprintf("ipc://%s", socketPath)
+	}
+
+	return
 }
